cf52238c53a3e2a9f659f49cf8ccdb4135637dd8
Avi-cyber <apbusiness5762@gmail.com>
2020-07-13 22:02:26 +0530

    Average

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double Result;
+            Console.WriteLine("Enter 3 numbers");
+            a = Convert.ToInt16(Console.ReadLine());
+            b = Convert.ToInt16(Console.ReadLine());
+            c = Convert.ToInt16(Console.ReadLine());
+
+            Result =(double) (a + b + c) / 3;
+            Console.WriteLine(Result);
+        }
+    }
+}
